e6517db53d8b7791b5690c9f7ffbce0c6bf5e9f8
Bruce Alexsander <brucealexsanderalves@gmail.com>
2019-02-26 18:50:41 -0300

    Add project files.

---
+++ TestePratico2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestePratico2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
